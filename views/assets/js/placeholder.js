f8047d8de00b111eb21c8ee1831fa0d5423648ed
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-15 15:08:15 +0530

    Light CheckBoxes

--- views/assets/js/placeholder.js
+++ views/assets/js/placeholder.js
@@ -1,7 +1,21 @@
 
-$(document).ready(function(){
-	
-	$(".form-username").val("Username...");
-	$(".form-password").val("Password...");
-	
-});
+function addGlow(checkId,checkmain) {
+  console.log(checkId);
+  // console.log("\'" + checkId.toString() + "\'");
+  // document.getElementById("\'" + checkId.toString() + "\'").checked = true;
+  // console.log('Checked');
+  if(checkmain[0].checked == true){
+    for(var i =0; i< checkId.length; i++){
+      checkId[i].checked = true;
+      checkmain[i].checked = true;
+    }
+  }
+  // } checkId[0].checked = true;
+  else {
+    // for(var i =0; i< checkId.length; i++){
+    //   checkId[i].checked = false;
+    //   checkmain[i].checked = false;
+    // }
+    checkId[0].checked = false;
+  }
+}
